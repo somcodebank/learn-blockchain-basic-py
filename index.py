ff701cd7bd780b9a7f30d31d0cf1522a5c560927
somcodebank <som.github@gmail.com>
2020-05-16 16:47:18 -0700

    Block class

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print('1001')
+class Block:
+    def __init__(self, data, hash, last_hash):
+        self.data = data
+        self.hash = hash
+        self.last_hash = last_hash
+
+
+one_block = Block('one_data', 'one_hash', 'one_last_hash')
+
+print(one_block.data)
+print(one_block.hash)
+print(one_block.last_hash)
