105d4206cc8c45c89802ad1c355a7ee48e3a50b8
somcodebank <som.github@gmail.com>
2020-05-16 16:59:04 -0700

    include pseudo hash function

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
+def pseudo_hash(data):
+    return '<hash>' + data + '</hash>'
+
+
+def hash(data):
+    return pseudo_hash(data)
+
+
 class Block:
-    def __init__(self, data, hash, last_hash):
+    def __init__(self, data, hash_data, last_hash_data):
         self.data = data
-        self.hash = hash
-        self.last_hash = last_hash
+        self.hash = hash_data
+        self.last_hash = last_hash_data
 
 
-one_block = Block('one_data', 'one_hash', 'one_last_hash')
+one_block = Block('one_data', hash('one_hash'), 'one_last_hash')
 
 print(one_block.data)
 print(one_block.hash)
